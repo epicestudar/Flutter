8a2b7ad36bb727b3ce8ccfd368dc4a77ab75ed3a
epicestudar <viniciusftza@gmail.com>
2024-02-15 16:09:29 -0300

    variaveis e operadores

---
+++ variaveis/android/app/src/main/kotlin/com/example/variaveis/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.variaveis
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ variaveis/lib/main.dart
@@ -0,0 +1,11 @@
+void main() {
+  String nome = "João";
+  int idade = 25;
+  double altura = 1.75;
+  bool estudante = true;
+
+  print("Nome: $nome");
+  print("Idade: $idade anos");
+  print("Altura: $altura");
+  print("Estudante: $estudante");
+}
