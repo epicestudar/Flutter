b8bb7d458aa4bc8b6f85de87e88c1e7f519e2235
epicestudar <viniciusftza@gmail.com>
2024-02-16 14:37:52 -0300

    switch case

---
+++ calculadora2/lib/main.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  runApp(CalculadoraApp());
+}
+
+class CalculadoraApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Calculadora(),
+    );
+  }
+}
+
+class Calculadora extends StatefulWidget {
+  @override
+  _CalculadoraState createState() => _CalculadoraState();
+}
+
+class _CalculadoraState extends State<Calculadora> {
+  TextEditingController _controllerNumero1 = TextEditingController();
+  TextEditingController _controllerNumero2 = TextEditingController();
+  String _resultado = '';
+
+  void _calcular(String operacao) {
+    double numero1 = double.tryParse(_controllerNumero1.text) ?? 0.0;
+    double numero2 = double.tryParse(_controllerNumero2.text) ?? 0.0;
+    double? resultado;
+
+    setState(() {
+      switch (operacao) {
+        case "Somar":
+          resultado = numero1 + numero2;
+          break;
+
+        case "Sub":
+          resultado = numero1 - numero2;
+          break;
+
+        case "Mult":
+          resultado = numero1 * numero2;
+          break;
+        default:
+        resultado = numero2 != 0 ? numero1 / numero2 : null;
+      }
+      _resultado =
+          resultado != null ? 'Resultado: $resultado' : 'Erro na operação';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Calculadora Flutter'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            TextField(
+              controller: _controllerNumero1,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(labelText: 'Número 1'),
+            ),
+            SizedBox(height: 16.0),
+            TextField(
+              controller: _controllerNumero2,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(labelText: 'Número 2'),
+            ),
+            SizedBox(height: 10.0),
+            ElevatedButton(
+              onPressed: () => _calcular('Somar'),
+              child: Text('Somar'),
+            ),
+            SizedBox(height: 10.0),
+            ElevatedButton(
+              onPressed: () => _calcular('Sub'),
+              child: Text('Subtrair'),
+            ),
+            SizedBox(height: 10.0),
+            ElevatedButton(
+              onPressed: () => _calcular('Mult'),
+              child: Text('Multiplicar'),
+            ),
+            SizedBox(height: 10.0),
+            ElevatedButton(
+              onPressed: () => _calcular('Div'),
+              child: Text('Divisão'),
+            ),
+            SizedBox(height: 16.0),
+            Text(_resultado,
+                style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold)),
+          ],
+        ),
+      ),
+    );
+  }
+}
