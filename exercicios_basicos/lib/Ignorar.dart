7c37f0b010e16de55a2c1ddaee79bb75caf9792b
epicestudar <viniciusftza@gmail.com>
2024-03-11 16:50:12 -0300

    fazendo exercicios de interface

---
+++ exercicios_basicos/lib/Ignorar.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Menu Hamburger'),
+        ),
+        body: Container(
+          width: double.infinity,
+          height: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('olá, mundo')
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
