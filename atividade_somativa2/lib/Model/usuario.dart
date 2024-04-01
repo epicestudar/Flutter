a788290adba03d77fa15183bc52275f9d293f6f0
epicestudar <viniciusftza@gmail.com>
2024-04-01 16:55:39 -0300

    inicio do projeto

---
+++ atividade_somativa2/lib/Model/usuario.dart
@@ -0,0 +1,15 @@
+class UsuarioModel {
+  int id;
+  String nome;
+  String email;
+  String senha;
+  String telefone;
+
+  UsuarioModel({
+    required this.id,
+    required this.nome,
+    required this.email,
+    required this.senha,
+    required this.telefone,
+  });
+}
